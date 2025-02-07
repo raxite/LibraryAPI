0219509f8cab1b96eb4a7f16a61c55085dcce665
Kühnlein, Philip <philip.kuehnlein@student.fhws.de>
2025-02-07 15:44:15 +0100

    Remove unused import for ComponentScan in LibraryApiApplication

--- src/main/java/de/thws/libraryapi/LibraryApiApplication.java
+++ src/main/java/de/thws/libraryapi/LibraryApiApplication.java
@@ -2,7 +2,7 @@ package de.thws.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class LibraryApiApplication {
