3e7d4382c3303b5aa73f3651b6ddf952a654bb2e
Kühnlein, Philip <philip.kuehnlein@student.fhws.de>
2025-02-07 15:43:39 +0100

    Remove unnecessary ComponentScan annotation from LibraryApiApplication

--- src/main/java/de/thws/libraryapi/LibraryApiApplication.java
+++ src/main/java/de/thws/libraryapi/LibraryApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "de.thws.libraryapi")
 public class LibraryApiApplication {
 
     public static void main(String[] args) {
